b0571c4e178d3b2a6b5d52ead52662a872e96378
MrPython887 <134967988+MrPython887@users.noreply.github.com>
2023-07-03 23:47:45 +0100

    Função para ordenar dados

--- Ficha de trabalho 5.py
+++ Ficha de trabalho 5.py
@@ -1,2 +1,19 @@
 import csv
 import matplotlib.pyplot as plt
+#função leitura
+def ler_ficheiro(nome_arquivo):
+    dados =[]
+    with open(nome_arquivo, newline="") as arquivo_csv:
+        leitor_csv = csv.reader(arquivo_csv)
+        for linha in leitor_csv:
+            dados.append(linha)
+    return dados
+
+def ordenar_bubblesort(dados):
+    n = len(dados)
+    for i in range(n):
+        for j in range(n - i - 1):
+            if int(dados[j][1]) > int(dados[j + 1][1]):
+                dados[j], dados[j + 1] = dados[j + 1], dados[j]
+    return dados
+
