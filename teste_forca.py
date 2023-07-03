8e28158b8c5a8fa8cba98d2f870a7e8ba3d42bee
MrPython887 <134967988+MrPython887@users.noreply.github.com>
2023-07-03 23:39:17 +0100

    Função de leitura

---
+++ teste_forca.py
@@ -0,0 +1,40 @@
+palavra = "tempestade"
+letras_inseridas = []
+tentativas = 5
+ganhou = False
+
+
+while True:
+    for letra in palavra:
+        if letra.lower() in letras_inseridas:
+            print(letra, end=" ")
+        else:
+            print("_", end =" ")
+    print("Você tem {}".format(tentativas))
+    
+
+    #logica do jogo
+
+tentativa = input("Escolha uma letras para a palavra secreta: ") 
+letras_inseridas.append(tentativa.lower())
+if tentativa.lower() not in palavra.lower():
+    tentativas -= 1
+
+ganhou = True
+
+for letra in palavra:
+        if letra.lower() not in letras_inseridas:
+             
+         ganhou = False    
+
+if tentativas == 0 or ganhou:
+
+    break
+
+if ganhou:
+
+            print("Parabéns, acertou a palavra secreta. A palavra secreta era: {}".format(palavra))
+
+else:
+
+    print("Você perdeu! A palavra secreta era: {}".format(palavra))
